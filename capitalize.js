69736c6fcda106093f7c034c789b77e32d56b68c
Sam Stone <146684601+samstone1171@users.noreply.github.com>
2023-10-26 10:19:20 +0900

    Update capitalize.js

--- capitalize.js
+++ capitalize.js
@@ -1,1 +1,10 @@
 const student = "kArThika";
+
+const capitalize = (word) => {
+  const firstLetter = word[0].toUppercase();
+  const remainder = word.substring(1).toLowercase();
+  return `${firstLetter}${remainder}`;
+};
+console.log(capitalize(student));
+const myName = capitalize(student);
+console.log(myName);
